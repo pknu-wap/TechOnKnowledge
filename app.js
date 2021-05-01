6490d1689ec8d624297961ebbbf02ef5fb2567b0
pp449 <71641127+pp449@users.noreply.github.com>
2021-05-02 00:40:05 +0900

    installed some packages

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`✔ Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
