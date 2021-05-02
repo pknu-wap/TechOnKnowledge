fcb7e8e2522731caeafb289e36176ebea020c278
pp449 <71641127+pp449@users.noreply.github.com>
2021-05-02 20:10:41 +0900

    connect to mongodb with using mongoose

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TechOnKnowledge
+
 2021_WEB_TEAM1
+
+## To do list
+
+- [ ] Use mongoDB(with mongoose)
+- [ ] User authentication(use passport)
+- [ ] Upload now lecture API
+- [ ] Add curriculum API
--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 import express from "express";
 
-const PORT = 4000;
-
 const app = express();
 
-const handleListening = () =>
-  console.log(`✔ Server running: http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
